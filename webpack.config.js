575bd3c7feef8ec2bd80987b139c2a2aca740429
Mathis Bullinger <mathis@bullinger.dev>
2022-03-13 11:08:45 +0100

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { DefinePlugin } = require('webpack')
+const path = require('path')
+const fs = require('fs')
+
+module.exports = (_, argv) => {
+  const IS_DEV = argv.mode === 'development'
+
+  // text-replace any process.env.* variable at build time with the corresponding
+  // value found in the .env file if it exists or otherwise with the environment
+  // variable of the same name
+  const dotEnvPath = path.join(__dirname, '.env')
+  const dotEnv = fs.existsSync(dotEnvPath)
+    ? Object.fromEntries(
+        fs
+          .readFileSync(dotEnvPath, 'utf-8')
+          .split('\n')
+          .filter(v => v.includes('='))
+          .map(v => v.split('='))
+      )
+    : {}
+  const env = Object.fromEntries(
+    Object.entries({ ...process.env, ...dotEnv }).map(([k, v]) => [
+      `process.env.${k}`,
+      JSON.stringify(v),
+    ])
+  )
+
+  return {
+    mode: IS_DEV ? 'development' : 'production',
+    entry: {
+      app: path.join(__dirname, 'src/index.tsx'),
+    },
+    output: {
+      filename: '[name].js',
+      path: path.join(__dirname, 'build'),
+      publicPath: '/',
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      modules: [
+        path.join(__dirname, 'src'),
+        path.join(__dirname, 'node_modules'),
+      ],
+      alias: {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        // 'react-dom': 'preact/compat',
+        'react/jsx-runtime': 'preact/jsx-runtime',
+      },
+    },
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+    },
+    ...(IS_DEV && { devtool: 'source-map' }),
+    module: {
+      rules: [
+        {
+          test: /\.(js|ts)x?$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+        {
+          test: /\.sa|css$/,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.js$/,
+          enforce: 'pre',
+          use: ['source-map-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'index.html'),
+      }),
+      new MiniCssExtractPlugin(),
+      new DefinePlugin(env),
+    ],
+  }
+}
