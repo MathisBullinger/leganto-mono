35fcd3aa9a63366df0dc7092f0f6bf22ffadd2bc
Mathis Bullinger <mathis@bullinger.dev>
2022-03-26 12:41:30 +0100

    handle get requests

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
 import { schema } from './graphql'
 import { graphql } from 'graphql'
+import { oneOf } from 'facula'
 import type {
   APIGatewayEvent,
   Handler,
@@ -16,22 +17,45 @@ export const handler: Handler<
   APIGatewayEvent,
   APIGatewayProxyResult
 > = async event => {
+  let request: Record<string, string>
+
   if (event.httpMethod === 'POST' && typeof event.body === 'string') {
-    const parsed = JSON.parse(event.body)
-    console.log(parsed)
-
-    const response = await resolve(parsed.query, parsed.operationName)
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response),
+    try {
+      request = JSON.parse(event.body)
+    } catch (e) {
+      console.error('failed to parse query', e)
+      return respond(400)
     }
   }
 
-  await new Promise(res => res(''))
+  if (event.httpMethod === 'GET') {
+    request = event.queryStringParameters
+  }
 
-  return { statusCode: 200, body: 'test' }
+  if (!isValid(request)) return respond(400)
+  return await resolve(request.query, request.operationName)
 }
 
-const resolve = async (source: string, operationName?: string) =>
-  await graphql({ schema, source, operationName, rootValue })
+const isValid = (
+  request: any
+): request is { query: string; operationName?: string } => {
+  if (typeof request !== 'object' || request === null) return false
+  if (typeof request.query !== 'string') return false
+  if (oneOf(request.operationName, null, undefined))
+    delete request.operationName
+  if ('operationName' in request && typeof request.operationName !== 'string')
+    return false
+  return true
+}
+
+const resolve = async (source: string, operationName?: string) => {
+  try {
+    const result = await graphql({ schema, source, operationName, rootValue })
+    return respond(200, JSON.stringify(result))
+  } catch (e) {
+    console.error('failed to resolve query', e)
+    return respond(500)
+  }
+}
+
+const respond = (statusCode: number, body?: string) => ({ statusCode, body })
