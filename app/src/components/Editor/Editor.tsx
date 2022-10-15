16141ec444d69fc6e8fde1fd9dc494ce93703ad2
Mathis Bullinger <mathis@bullinger.dev>
2022-10-15 14:36:35 +0200

    tab drag animation

--- app/src/components/Editor/Editor.tsx
+++ app/src/components/Editor/Editor.tsx
@@ -7,15 +7,19 @@ import Pane from './EditorPane'
 import style from './Editor.module.scss'
 
 const EditorWrapper: FC = () => {
-  const [languages, setLanguages] = useState<LangCode[]>(['en', 'es'])
+  const [languages, setLanguages] = useState<LangCode[]>(['de', 'en', 'es'])
   const [highlighted, setHighlighted] = useState<LangCode | null>(null)
-
-  console.log({ highlighted })
 
   return (
     <div className={cn(style.wrapper, { [style.highlight]: highlighted })}>
       {createPortal(
-        <LanguageSelect languages={languages} onHighlight={setHighlighted} />,
+        <LanguageSelect
+          languages={languages}
+          onHighlight={setHighlighted}
+          onMove={(index, offset) => {
+            setLanguages(move(languages, index, index + offset))
+          }}
+        />,
         document.getElementById('custom-header-content')!
       )}
       {languages.map(lang => (
@@ -26,3 +30,8 @@ const EditorWrapper: FC = () => {
 }
 
 export default EditorWrapper
+
+const move = <T,>(list: T[], from: number, to: number): T[] => {
+  const tmp = [...list.slice(0, from), ...list.slice(from + 1)]
+  return [...tmp.slice(0, to), list[from], ...tmp.slice(to)]
+}
--- app/src/components/Editor/LanguageSelection.tsx
+++ app/src/components/Editor/LanguageSelection.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import { LangCode, languageName } from 'utils/language'
 
 import style from './LanguageSelection.module.scss'
@@ -6,21 +6,144 @@ import style from './LanguageSelection.module.scss'
 type Props = {
   languages: LangCode[]
   onHighlight: (lang: LangCode | null) => void
+  onMove: (index: number, offset: number) => void
 }
 
-const LanguageSelection: FC<Props> = ({ languages, onHighlight }) => {
+const LanguageSelection: FC<Props> = ({ languages, onHighlight, onMove }) => {
+  const [offsets, setOffsets] = useState<[left: number[], right: number[]]>()
+  const [initialIndex, setInitialIndex] = useState<number>(0)
+  const [currentIndex, setCurrentIndex] = useState<number>(0)
+  const [initialX, setInitialX] = useState(0)
+  const [gap, setGap] = useState(0)
+
   return (
-    <ol className={style.bar}>
-      {languages.map(code => (
-        <li
-          key={code}
-          onMouseEnter={() => onHighlight(code)}
-          onMouseLeave={() => onHighlight(null)}
-        >
-          {languageName[code]}
-        </li>
-      ))}
-    </ol>
+    <>
+      <ol
+        className={style.bar}
+        onDragStart={e => {
+          e.currentTarget.classList.toggle(style.rearrange, true)
+          e.dataTransfer.setDragImage(
+            document.getElementById(style.shadow)!,
+            0,
+            0
+          )
+        }}
+        onDragEnd={({ currentTarget }) =>
+          currentTarget.classList.toggle(style.rearrange, false)
+        }
+      >
+        {languages.map((code, i) => (
+          <li
+            key={`${code}-${i}`}
+            onMouseEnter={() => onHighlight(code)}
+            onMouseLeave={() => onHighlight(null)}
+            draggable="true"
+            onDragStart={({ currentTarget, clientX }) => {
+              currentTarget.classList.toggle(style.dragged, true)
+              currentTarget.style.zIndex = '2'
+
+              const items = [...currentTarget.parentElement!.children]
+              if (items.length < 2) return
+
+              const ownIndex = items.indexOf(currentTarget)
+
+              const itemBoxes = new Map(
+                items.map(item => [item, item.getBoundingClientRect()])
+              )
+
+              const gap =
+                itemBoxes.get(items[1])!.left - itemBoxes.get(items[0])!.right
+              setGap(gap)
+
+              const ownOffset =
+                itemBoxes.get(currentTarget)!.width / 2 + gap / 2
+
+              const nextOffsets = [...Array(items.length - 1 - ownIndex)].map(
+                (_, i) =>
+                  i === 0
+                    ? ownOffset
+                    : itemBoxes.get(items[ownIndex + i])!.width + gap
+              )
+
+              const previousOffsets = [...Array(ownIndex)].map((_, i) =>
+                i === 0
+                  ? -ownOffset
+                  : -(itemBoxes.get(items[ownIndex - i])!.width + gap)
+              )
+
+              const offsets = [previousOffsets, nextOffsets].map(offsets =>
+                offsets.map((_, i) =>
+                  offsets.slice(0, i + 1).reduce((a, c) => a + c, 0)
+                )
+              ) as [number[], number[]]
+
+              setOffsets(offsets)
+              setInitialX(clientX)
+              setInitialIndex(ownIndex)
+              setCurrentIndex(ownIndex)
+            }}
+            onDrag={async ({ clientX, currentTarget }) => {
+              if (!clientX || !offsets) return
+
+              const dx = clientX - initialX
+
+              const offsetList = offsets[+(dx > 0)]
+              const findLastIndex = (offsetList as any).findLastIndex.bind(
+                offsetList
+              ) as number[]['findIndex']
+
+              const offsetIndex = findLastIndex(v =>
+                dx > 0 ? v <= dx : v >= dx
+              )
+
+              const index = initialIndex + (offsetIndex + 1) * (dx > 0 ? 1 : -1)
+
+              if (index === currentIndex) return
+
+              setCurrentIndex(index)
+
+              const items = [
+                ...currentTarget.parentElement!.children,
+              ] as HTMLElement[]
+
+              const elementOffsets = new Map(items.map(item => [item, 0]))
+
+              const { width } = currentTarget.getBoundingClientRect()
+
+              const step = Math.sign(index - initialIndex)
+              for (let i = initialIndex + step; i !== index + step; i += step) {
+                elementOffsets.set(items[i], (width + gap) * -step)
+
+                elementOffsets.set(
+                  currentTarget,
+                  elementOffsets.get(currentTarget)! +
+                    (items[i].getBoundingClientRect().width + gap) * step
+                )
+              }
+
+              ;[...elementOffsets].forEach(([element, offset]) => {
+                element.style.transform = `translateX(${offset}px)`
+              })
+            }}
+            onDragEnd={async ({ currentTarget }) => {
+              ;[...currentTarget.parentElement!.children].forEach(item => {
+                item.classList.toggle(style.dragged, false)
+              })
+
+              if (initialIndex !== currentIndex)
+                onMove(initialIndex, currentIndex - initialIndex)
+            }}
+          >
+            {languageName[code]}
+          </li>
+        ))}
+      </ol>
+      <img
+        src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
+        id={style.shadow}
+        alt=""
+      />
+    </>
   )
 }
 
