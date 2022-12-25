39ff6b7af09359d5ee20bc9a45ccb20cd211e12f
Mathis Bullinger <mathis@bullinger.dev>
2022-12-25 20:21:48 +0100

    mobile pane layout

--- app/src/pages/Text.tsx
+++ app/src/pages/Text.tsx
@@ -1,8 +1,9 @@
-import { useMemo, VFC, CSSProperties, ReactHTML } from 'react'
+import { useMemo, VFC, CSSProperties, ReactHTML, useState, useRef } from 'react'
 import { RouteProps, Redirect } from 'itinero'
 import { QueryResult, useQuery } from 'api/hooks'
 import Icon from 'components/Icon'
 import style from './Text.module.scss'
+import { useEffect } from 'preact/hooks'
 
 const TextWrapper: VFC<RouteProps<{}, { id: string; lang: string }>> = ({
   match,
@@ -18,7 +19,7 @@ const TextWrapper: VFC<RouteProps<{}, { id: string; lang: string }>> = ({
   })
 
   return (
-    <div className={style.page}>
+    <div>
       {loading ? (
         <Icon type="loadingDots" />
       ) : !data?.text ? (
@@ -33,44 +34,103 @@ const TextWrapper: VFC<RouteProps<{}, { id: string; lang: string }>> = ({
 export default TextWrapper
 
 const TextView: VFC<Text> = ({ translations }) => {
-  const sections = translations.map(({ language, title, content }) => ({
-    language,
-    title,
-    content: [
-      ...new DOMParser().parseFromString(content ?? '', 'text/html').body
-        .children,
-    ],
-  }))
+  const [paneHeights, setPaneHeights] = useState<Record<string, number>>({})
+  const textHeight = Math.max(...Object.values(paneHeights))
+
+  const sections = useMemo(
+    () =>
+      translations.map(({ language, title, content }) => ({
+        language,
+        title,
+        content: [
+          ...new DOMParser().parseFromString(content ?? '', 'text/html').body
+            .children,
+        ],
+      })),
+    [translations]
+  )
 
   return (
     <article
       className={style.textWrapper}
-      style={{ '--columns': sections.length } as CSSProperties}
-    >
-      {sections.map(({ language, title, content }, column) => (
-        <section
-          key={language}
-          lang={language}
-          style={{ '--column': column } as CSSProperties}
-        >
-          <h1 style={{ '--row': 0 } as CSSProperties}>{title}</h1>
-          {content.map(({ tagName, innerHTML }, i) => {
-            const Tag = tagName.toLowerCase() as keyof ReactHTML
+      style={{ '--tabs': sections.length } as CSSProperties}
+      onScroll={({ currentTarget: el }) => {
+        const dy = el.scrollTop / (el.scrollHeight - el.offsetHeight)
 
-            return (
-              <Tag
-                key={i}
-                style={{ '--row': i + 2 } as CSSProperties}
-                dangerouslySetInnerHTML={{ __html: innerHTML }}
-              />
-            )
-          })}
+        const panes = ([...el.children] as HTMLElement[])
+          .slice(0, -1)
+          .map(v => v.firstChild as HTMLElement)
+
+        panes.forEach(pane => {
+          pane.style.transform = `translateY(${
+            -dy * (pane.offsetHeight - pane.parentElement!.offsetHeight)
+          }px)`
+        })
+      }}
+    >
+      {sections.map((pane, column) => (
+        <section
+          key={pane.language}
+          lang={pane.language}
+          style={{ '--tab': column } as CSSProperties}
+        >
+          <Content
+            {...pane}
+            onHeightChange={height =>
+              setPaneHeights(v => ({ ...v, [pane.language]: height }))
+            }
+          />
         </section>
       ))}
+      <div className={style.shadow} style={{ minHeight: `${textHeight}px` }} />
     </article>
   )
 }
 
-// const useText = ()
+const Content: VFC<
+  Translation & { onHeightChange: (height: number) => void }
+> = ({ title, content, onHeightChange }) => {
+  const heightCbRef = useRef(onHeightChange)
+  heightCbRef.current = onHeightChange
+
+  const observer = useMemo(
+    () =>
+      new ResizeObserver(([{ contentRect }]) => {
+        heightCbRef.current?.(contentRect.height)
+      }),
+    []
+  )
+
+  useEffect(
+    () => () => {
+      observer.disconnect()
+      heightCbRef.current?.(0)
+    },
+    [observer]
+  )
+
+  return (
+    <div className={style.content} ref={el => el && observer.observe(el)}>
+      <h1 style={{ '--row': 0 } as CSSProperties}>{title}</h1>
+      {content.map(({ tagName, innerHTML }, i) => {
+        const Tag = tagName.toLowerCase() as keyof ReactHTML
+
+        return (
+          <Tag
+            key={i}
+            style={{ '--row': i + 2 } as CSSProperties}
+            dangerouslySetInnerHTML={{ __html: innerHTML }}
+          />
+        )
+      })}
+    </div>
+  )
+}
 
 type Text = Exclude<QueryResult<'getText'>['text'], null | undefined>
+
+type Translation = {
+  language: string
+  title?: string | null
+  content: Element[]
+}
